f0717d9dfaf4df1e1e0c36a91905b1be95718f2e
Katie Liu <katieliu59@gmail.com>
2023-04-24 15:13:11 -0400

    Create togglePlay function and event listeners for play/pause

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,13 +1,28 @@
-// Get elements
+/* Get elements */
 
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
-// Build functions 
+/* Build functions */
 
-// Hook up event listeners
+function togglePlay() {
+    //if video is paused -> play it, else (video is playing) -> pause it when toggle fucntion is called
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+/* Hook up event listeners */
+
+//when click on video display, run togglePlay function
+video.addEventListener('click', togglePlay);
+
+//when click on play/pause button, run togglePlay function
+toggle.addEventListener('click', togglePlay)
